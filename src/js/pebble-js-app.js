a84fadaa850f0d38c97e9b5d127772cec2d2ccd0
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2014-03-09 01:50:40 -0800

    First Working Version!
    Problems: Values displayed in ints, values dont refresh

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,40 +1,3 @@
-function sett() {
-  console.log("#Set method called");
-  
-  /*
-  var url = new XMLHttpRequest();
-  url.open('GET', 'http://www.cryptocoincharts.info/v2/api/listCoins', true);
-  url.onload = function(e) {
-    console.log("in onload");
-    if(url.readyState == 4 && url.status == 200) {
-      var response = JSON.parse(url.responseText);
-      
-      var name = response[2].name;
-      var price = response[2].price_btc;
-      var volume = response[2].volume_btc;
-      
-      Pebble.sendAppMessage({
-        "name": name,
-         "price": price,
-         "volume": volume,
-      });
-    }
-  };
-  url.send(null);
-  */
-  var response = HTTPGET("https://api.bitcoinaverage.com/all");
-  
-  var json = JSON.parse(response);
-  
-  var price = json.USD.averages.bid;
-  console.log("price: " + price);
-  
-  var dict = {"KEY_PRICE" : price};
-     
-  // Send data to watch for display
-  Pebble.sendAppMessage(dict);
-}
-
 function HTTPGET(url) {
     var req = new XMLHttpRequest();
     req.open("GET", url, false);
@@ -42,16 +5,27 @@ function HTTPGET(url) {
     return req.responseText;
 }
 
+var sett = function() {
+  console.log("#Set method called");
+  
+  var response = HTTPGET("https://api.bitcoinaverage.com/all");
+  
+  var json = JSON.parse(response);
+  
+  var last = json.USD.averages.last;
+  console.log("last: " + last);
+  
+  var bid = json.USD.averages.bid;
+  console.log("bid: " + bid);
+  
+  var dict = {"KEY_LAST" : last, "KEY_BID" : bid};
+     
+  // Send data to watch for display
+  Pebble.sendAppMessage(dict);
+};
+
 Pebble.addEventListener("ready",
   function(e) {
-    console.log("JavaScript app ready and running!");
     sett();
-  }
-);
-
-Pebble.addEventListener("appmessage",
-  function(e) {
-	//Watch wants new data!
-	sett();
   }
 );
--- src/main.c
+++ src/main.c
@@ -1,24 +1,34 @@
 #include <pebble.h>
+  
+Window* window;
 
-Window *window;
-TextLayer *currency_layer, *price_layer, *time_layer;
-char price_buffer[64], time_buffer[32];
+TextLayer *currency_layer, *last_layer, *bid_layer, *time_layer;
+char last_buffer[64], bid_buffer[64], time_buffer[32];
 
 enum {
-  KEY_PRICE = 0,
+  KEY_LAST = 0,
+  KEY_BID = 1,
 };
 
-void process_tuple(Tuple *t) {
+void process_tuple(Tuple *t)
+{
+  //Get key
   int key = t->key;
-  
-  // Get integer value, if present
+ 
+  //Get double value, if present
   int value = t->value->int32;
  
+  //Decide what to do
   switch(key) {
-    // Price received
-    case KEY_PRICE:
-      snprintf(price_buffer, sizeof("Price: XX \u00B0C"), "Price: %d \u00B0C", value);
-      text_layer_set_text(price_layer, (char*) &price_buffer);
+    case KEY_LAST:
+      //Last received
+      snprintf(last_buffer, sizeof("100000000000000"), "Last: %d", value);
+      text_layer_set_text(last_layer, (char*) &last_buffer);
+      break;
+    case KEY_BID:
+      //Bid received
+      snprintf(bid_buffer, sizeof("100000000000000"), "Bid: %d", value);
+      text_layer_set_text(bid_layer, (char*) &bid_buffer);
       break;
   }
  
@@ -29,119 +39,89 @@ void process_tuple(Tuple *t) {
   text_layer_set_text(time_layer, (char*) &time_buffer);
 }
 
-// clicking stuffs
-void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_background_color(price_layer, GColorBlack);
-}
-   
-void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_background_color(price_layer, GColorWhite);
-}
+
+static TextLayer* init_text_layer(GRect location, GColor colour, GColor background, const char *res_id, GTextAlignment alignment)
+{
+  TextLayer *layer = text_layer_create(location);
+  text_layer_set_text_color(layer, colour);
+  text_layer_set_background_color(layer, background);
+  text_layer_set_font(layer, fonts_get_system_font(res_id));
+  text_layer_set_text_alignment(layer, alignment);
  
-void select_click_handler(ClickRecognizerRef recognizer, void *context) { 
-  vibes_short_pulse();
+  return layer;
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-}
-
-static void in_received_handler(DictionaryIterator *iter, void *context) {
-  
-  (void) context;
-	
-	//Get data
-	Tuple *t = dict_read_first(iter);
-	if(t)
-	{
-		process_tuple(t);
-	}
-	
-	//Get next
-	while(t != NULL)
-	{
-		t = dict_read_next(iter);
-		if(t)
-		{
-			process_tuple(t);
-		}
-	}
-}
-
-void send_int(uint8_t key, uint8_t cmd) {
-    DictionaryIterator *iter;
-    app_message_outbox_begin(&iter);
- 	
- 	  Tuplet value = TupletInteger(key, cmd);
- 	  dict_write_tuplet(iter, &value);
- 	
-   	app_message_outbox_send();
-}
-
-void tick_callback(struct tm *tick_time, TimeUnits units_changed) {
-  // Every one minute
-	if(tick_time->tm_min % 1 == 0)
-	{
-		//Send an arbitrary message, the response will be handled by in_received_handler()
-		send_int(5, 5);
-	}
-}
-
-void handle_init(void) {
-	// Create a window and text layer
-	window = window_create();
-	currency_layer = text_layer_create(GRect(0, 0, 144, 154));
-  price_layer = text_layer_create(GRect(0, 65, 144, 250));
-  time_layer = text_layer_create(GRect(0, 120, 144, 100));
-	
+void window_load(Window *window)
+{
+	currency_layer = init_text_layer(GRect(0, 0, 144, 154), GColorBlack, GColorClear, "RESOURCE_ID_BITHAM_42_BOLD", GTextAlignmentCenter);
 	text_layer_set_text(currency_layer, "Bitcoin");
-	text_layer_set_font(currency_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_background_color(currency_layer, GColorClear);
-	text_layer_set_text_alignment(currency_layer, GTextAlignmentCenter);
-  
-  text_layer_set_text(price_layer, "N/A");
-	text_layer_set_font(price_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
-	text_layer_set_text_alignment(price_layer, GTextAlignmentCenter);
-  
-  text_layer_set_text(time_layer, "Last Updated: ");
-	text_layer_set_font(time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-	text_layer_set_text_alignment(time_layer, GTextAlignmentCenter);
-	
-	// Add the text layer to the window
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(currency_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(price_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
-  
-  // Register AppMessage events
-	app_message_register_inbox_received(in_received_handler);					 
-	app_message_open(512, 512);		//Large input and output buffer sizes
-	
-	//Register to receive minutely updates
-	tick_timer_service_subscribe(MINUTE_UNIT, tick_callback);
-  
-  // Clicker
-  window_set_click_config_provider(window, click_config_provider);
-  
-  // Push the window
-	window_stack_push(window, true);
+
+	last_layer = init_text_layer(GRect(0, 60, 144, 250), GColorBlack, GColorClear, "RESOURCE_ID_GOTHIC_28", GTextAlignmentCenter);
+	text_layer_set_text(last_layer, "Last: N/A");
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(last_layer));
+
+	bid_layer = init_text_layer(GRect(0, 85, 144, 250), GColorBlack, GColorClear, "RESOURCE_ID_GOTHIC_28", GTextAlignmentCenter);
+	text_layer_set_text(bid_layer, "Bid: N/A");
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(bid_layer));
+
+	time_layer = init_text_layer(GRect(0, 140, 144, 100), GColorBlack, GColorClear, "RESOURCE_ID_GOTHIC_18", GTextAlignmentCenter);
+	text_layer_set_text(time_layer, "Last updated: N/A");
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
 }
 
-void handle_deinit(void) {
-	// Destroy the text layer
-	text_layer_destroy(currency_layer);
-  text_layer_destroy(price_layer);
-  text_layer_destroy(time_layer);
-	
-	// Destroy the window
-	window_destroy(window);
-  
-  tick_timer_service_unsubscribe();
+void window_unload(Window *window)
+{
+  text_layer_destroy(currency_layer);
+  text_layer_destroy(last_layer);
+  text_layer_destroy(bid_layer);
+  text_layer_destroy(time_layer); 
 }
 
-int main(void) {
-	handle_init();
-	app_event_loop();
-	handle_deinit();
+static void in_received_handler(DictionaryIterator *iter, void *context) 
+{
+  //Get data
+  Tuple *t = dict_read_first(iter);
+  if(t)
+  {
+    process_tuple(t);
+  }
+   
+  //Get next
+  while(t != NULL)
+  {
+    t = dict_read_next(iter);
+    if(t)
+    {
+      process_tuple(t);
+    }
+  }
+}
+  
+void init()
+{
+  window = window_create();
+  WindowHandlers handlers = {
+    .load = window_load,
+    .unload = window_unload
+  };
+  window_set_window_handlers(window, handlers);
+ 
+  //Register AppMessage events
+  app_message_register_inbox_received(in_received_handler);           
+  app_message_open(512, 512);    //Large input and output buffer sizes
+  
+  window_stack_push(window, true);
+}
+  
+void deinit()
+{
+  window_destroy(window);
+}
+  
+int main(void)
+{
+  init();
+  app_event_loop();
+  deinit();
 }
