965b7cff72c15122d3883c03e9f30f3f8703d9f3
Arjun Gopisetty <arjun.gopisetty@gmail.com>
2014-03-08 23:14:38 -0800

    Edward help

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,6 +1,7 @@
 function sett() {
   console.log("#Set method called");
   
+  /*
   var url = new XMLHttpRequest();
   url.open('GET', 'http://www.cryptocoincharts.info/v2/api/listCoins', true);
   url.onload = function(e) {
@@ -11,35 +12,46 @@ function sett() {
       var name = response[2].name;
       var price = response[2].price_btc;
       var volume = response[2].volume_btc;
-      console.log(name + " " + price + " " + volume)
       
-      var dict = {"name": name, "price": price, "volume": volume,}
-      Pebble.sendAppMessage(dict);
+      Pebble.sendAppMessage({
+        "name": name,
+         "price": price,
+         "volume": volume,
+      });
     }
-  }
+  };
   url.send(null);
+  */
+  var response = HTTPGET("https://api.bitcoinaverage.com/all");
   
-  var burl = new XMLHttpRequest();
-  burl.open('GET', 'https://api.bitcoinaverage.com/all', true);
-  console.log("gg no re");
-  burl.onload = function(e) {
-    if(burl.readyState == 4 && burl.status == 200) {
-      var response = JSON.parse(burl.responseText);
-      
-      var price = response.USD.averages.bid;
-      console.log("price: " + price);
-      
-      var dict = {"KEY_PRICE": price}
-      Pebble.sendAppMessage(dict);
-    }
-  }
-  burl.send(null);
+  var json = JSON.parse(response);
+  
+  var price = json.USD.averages.bid;
+  console.log("price: " + price);
+  
+  var dict = {"KEY_PRICE" : price};
+     
+  // Send data to watch for display
+  Pebble.sendAppMessage(dict);
+}
+
+function HTTPGET(url) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url, false);
+    req.send(null);
+    return req.responseText;
 }
 
 Pebble.addEventListener("ready",
   function(e) {
     console.log("JavaScript app ready and running!");
-    
     sett();
+  }
+);
+
+Pebble.addEventListener("appmessage",
+  function(e) {
+	//Watch wants new data!
+	sett();
   }
 );
--- src/main.c
+++ src/main.c
@@ -4,12 +4,23 @@ Window *window;
 TextLayer *currency_layer, *price_layer, *time_layer;
 char price_buffer[64], time_buffer[32];
 
+enum {
+  KEY_PRICE = 0,
+};
+
 void process_tuple(Tuple *t) {
+  int key = t->key;
+  
   // Get integer value, if present
   int value = t->value->int32;
  
-  // Price received
-  text_layer_set_text(price_layer, (char*) &price_buffer);
+  switch(key) {
+    // Price received
+    case KEY_PRICE:
+      snprintf(price_buffer, sizeof("Price: XX \u00B0C"), "Price: %d \u00B0C", value);
+      text_layer_set_text(price_layer, (char*) &price_buffer);
+      break;
+  }
  
   //Set time this update came in
   time_t temp = time(NULL);  
@@ -37,52 +48,45 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
-void out_sent_handler(DictionaryIterator *sent, void *context) {
-   // outgoing message was delivered
-}
-
-
-void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
-   // outgoing message failed
-}
-
 static void in_received_handler(DictionaryIterator *iter, void *context) {
-  // Get data
-  Tuple *t = dict_read_first(iter);
-  if(t) {
-    process_tuple(t);
-  }
-   
-  // Get next
-  while(t != NULL) {
-    t = dict_read_next(iter);
-    if(t) {
-      process_tuple(t);
-    }
-  }
-}
-
-void in_dropped_handler(AppMessageResult reason, void *context) {
-   // incoming message dropped
+  
+  (void) context;
+	
+	//Get data
+	Tuple *t = dict_read_first(iter);
+	if(t)
+	{
+		process_tuple(t);
+	}
+	
+	//Get next
+	while(t != NULL)
+	{
+		t = dict_read_next(iter);
+		if(t)
+		{
+			process_tuple(t);
+		}
+	}
 }
 
 void send_int(uint8_t key, uint8_t cmd) {
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
-     
-    Tuplet value = TupletInteger(key, cmd);
-    dict_write_tuplet(iter, &value);
-     
-    app_message_outbox_send();
+ 	
+ 	  Tuplet value = TupletInteger(key, cmd);
+ 	  dict_write_tuplet(iter, &value);
+ 	
+   	app_message_outbox_send();
 }
 
 void tick_callback(struct tm *tick_time, TimeUnits units_changed) {
-  // Every five minutes
-  if(tick_time->tm_min % 5 == 0)
-  {
-    // Send an arbitrary message, the response will be handled by in_received_handler()
-    send_int(5, 5);
-  }
+  // Every one minute
+	if(tick_time->tm_min % 1 == 0)
+	{
+		//Send an arbitrary message, the response will be handled by in_received_handler()
+		send_int(5, 5);
+	}
 }
 
 void handle_init(void) {
@@ -111,14 +115,11 @@ void handle_init(void) {
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
   
   // Register AppMessage events
-  app_message_register_inbox_received(in_received_handler);           
-	app_message_register_inbox_dropped(in_dropped_handler); 
-	app_message_register_outbox_failed(out_failed_handler);
-  app_message_register_outbox_sent(out_sent_handler);
-  app_message_open(512, 512);
-  
-  // Register to receive minutely updates
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_callback);
+	app_message_register_inbox_received(in_received_handler);					 
+	app_message_open(512, 512);		//Large input and output buffer sizes
+	
+	//Register to receive minutely updates
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_callback);
   
   // Clicker
   window_set_click_config_provider(window, click_config_provider);
